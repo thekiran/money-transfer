dab54bf49f15a5c57aace943cd90a7d7bb4d6199
unknown <kirankumargs23.kkgs@gmail.com>
2020-11-03 14:41:01 +0530

    chart completed

---
+++ client/src/components/showcase/CustomChart.js
@@ -0,0 +1,70 @@
+// import {  } from 'chart.js'
+import React, { Component } from 'react'
+import { Bar, Line, Pie, defaults } from 'react-chartjs-2'
+
+defaults.global.legend.display = false
+defaults.scale.ticks.display = false
+console.log(defaults.global)
+class Chart extends Component {
+  componentDidMount() {
+    let data = {}
+  }
+  constructor(props) {
+    super(props)
+    this.state = {
+      chartData: props.chartData,
+    }
+  }
+
+  static defaultProps = {
+    displayTitle: false,
+    displayLegend: false,
+    legendPosition: 'right',
+    location: 'City',
+  }
+
+  render() {
+    return (
+      <div className='chart'>
+        <Line
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              text: 'Largest Cities In ' + this.props.location,
+              fontSize: 25,
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: this.props.legendPosition,
+            },
+            hover: {
+              mode: 'index',
+            },
+            layout: {
+              padding: {
+                left: 0,
+                right: 0,
+                top: 20,
+                bottom: 0,
+              },
+            },
+          }}
+        />
+      </div>
+    )
+  }
+
+  // displayName: 'LineExample',
+
+  //   render() {
+  //     return (
+  //       <div>
+  //         <h2>Line Example</h2>
+  //         <Line data={this.state.data} />
+  //       </div>
+  //     )
+  //   }
+}
+
+export default Chart
--- client/src/components/showcase/Showcase.js
+++ client/src/components/showcase/Showcase.js
@@ -1,11 +1,98 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Chart from './CustomChart'
 
 import './showcase.css'
 const Showcase = () => {
+  const data = {
+    chartData: {
+      labels: [
+        'Boston',
+        'Worcester',
+        'Springfield',
+        'Lowell',
+        'Cambridge',
+        'New Bedford',
+      ],
+      datasets: [
+        {
+          // label: 'Population',
+          data: [8000, 9000, 7000, 5000, 5000, 6000, 3000],
+          fill: true,
+          lineTension: 0.4,
+          backgroundColor: 'rgba(183, 161, 150,0.1)',
+          borderColor: 'rgba(255,255,255,1)',
+          borderCapStyle: '',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#000',
+          pointBorderWidth: 10,
+          pointHoverRadius: 10,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 10,
+          pointRadius: 0,
+          pointHitRadius: 100,
+          visible: false,
+          // data: [65, 59, 80, 81, 56, 55, 40],
+        },
+      ],
+      options: {
+        scales: {
+          xAxes: [
+            {
+              display: false,
+            },
+          ],
+          yAxes: [
+            {
+              display: false,
+            },
+          ],
+        },
+      },
+    },
+  }
+  useEffect(() => {
+    // console.log(Chart.defaults.globals.)
+    // console.log(Chart)
+    // setTimeout(() => {
+    // }, 1000)
+  }, [])
   return (
     <div className='showcase-container'>
       <div className='showcase-content'>
-        <div className='show-box'></div>
+        <div className='show-content'>
+          <h6 className='show-title'>We use the real exchange rate.</h6>
+          <div className='show-stats'>
+            <span>As of 1 min ago, 1 INR → 0.013 USD</span>
+            <span>
+              Mid market rate ?
+              <span className='circle'>
+                {/* ?<i class='fas fa-question'></i> */}
+              </span>
+            </span>
+          </div>
+        </div>
+        <div className='show-box'>
+          <Chart
+            chartData={data.chartData}
+            location='Massachusetts'
+            legendPosition='bottom'
+          />
+        </div>
+        <div className='btn-track'>Track this change</div>
+      </div>
+      <div className='text-container'>
+        <div className='show-text'>
+          <h6>No hidden fees.</h6>
+          <p>
+            We’re on a mission to bring transparency to finance. We charge as
+            little as possible, and we always show you upfront. No hidden fees.
+            No bad exchange rates. No surprises.
+          </p>
+        </div>
       </div>
     </div>
   )
